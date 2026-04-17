760c54f19d99616d9c3ed34744c5bbb3f5866bdd
nickmiztli <joseecarrillo79@gmail.com>
2026-04-16 22:52:24 -0600

    add changes from the datagen

--- src/main/java/net/miztli/techcraft/datagen/ModGlobalLootModifiersProvider.java
+++ src/main/java/net/miztli/techcraft/datagen/ModGlobalLootModifiersProvider.java
@@ -25,8 +25,6 @@ public class ModGlobalLootModifiersProvider extends GlobalLootModifierProvider {
                 LootItemBlockStatePropertyCondition.hasBlockStateProperties(Blocks.GRASS).build(),
                 LootItemRandomChanceCondition.randomChance( 0.35f).build()},ModItems.ONYX.get()));
 
-        add("sapphire_from_zombie" , new AddItemModifier(new LootItemCondition[] {
-                new  LootTableIdCondition.Builder(new ResourceLocation("entities/zombie")).build()}, ModItems.SAPPHIRE.get()));
 
         add("metal_detector_from_jungle_temples" , new AddItemModifier(new LootItemCondition[] {
                 new  LootTableIdCondition.Builder(new ResourceLocation("chests/jungle_temple")).build()}, ModItems.PROSPECTOR_PICK.get()));
